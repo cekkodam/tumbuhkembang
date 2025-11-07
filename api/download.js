828436c5c99067fbafd578c2c918489ef1d5ea24
cekkodam <farhanrisqi17@gmail.com>
2025-11-08 03:15:52 +0700

    Update download.js

--- api/download.js
+++ api/download.js
@@ -2,15 +2,14 @@ import fs from "fs";
 
 export default function handler(req, res) {
   const { file } = req.query;
-  const filePath = `/tmp/${file}`;
+  const path = `/tmp/${file}`;
 
-  if (!fs.existsSync(filePath)) {
-    return res.status(404).send("File not found");
+  if (!fs.existsSync(path)) {
+    return res.status(404).json({ error: "File not found" });
   }
 
   res.setHeader("Content-Type", "application/x-subrip");
   res.setHeader("Content-Disposition", `attachment; filename="${file}"`);
 
-  const stream = fs.createReadStream(filePath);
-  stream.pipe(res);
+  fs.createReadStream(path).pipe(res);
 }
